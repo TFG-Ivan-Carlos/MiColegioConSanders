79ad3e73bdbaf3eaebddd0e29df907b9c4873e25
ivan-rodnov <57326596+ivan-rodnov@users.noreply.github.com>
2021-06-11 11:16:35 +0200

    limpieza de variables que no se usan

--- app/src/main/java/com/tfg/controlparental/MainActivity.java
+++ app/src/main/java/com/tfg/controlparental/MainActivity.java
@@ -19,67 +19,9 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text;
-    RequestQueue requestQueue;
-    private static final String url= "https://developer.android.com/training/volley/request";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        requestQueue = Volley.newRequestQueue(this);
-
-        initUI();
-        //stringRequest();
-        jsonArrayRequest();
     }
 
-    private void initUI(){
-        //text = findViewById(R.id.textUser);
-    }
-
-    private void stringRequest(){
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                text.setText(response);
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-        requestQueue.add(request);
-    }
-
-    private void jsonArrayRequest(){
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
-                Request.Method.GET,
-                url,
-                null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        int size = response.length();
-                        for(int i = 0; i < size; i++){
-                            try {
-                                JSONObject jsonObject = new JSONObject(response.get(i).toString());
-                                String title = jsonObject.getString("title");
-                                text.append("Title: " + title + '\n');
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }
-        );
-        requestQueue.add(jsonArrayRequest);
-    }
 }
